0e687e5450eae83cde461d66acf2f7b829f89b01
Priyank Rajai <priyank73@hotmail.com>
2024-08-04 15:08:00 +0530

    feat: adding types for table

---
+++ src/components/data-table/types.ts
@@ -0,0 +1,25 @@
+export interface SearchParams {
+  [key: string]: string | string[] | undefined;
+}
+
+export interface Option {
+  label: string;
+  value: string | boolean;
+}
+
+export interface DataTableFilterField<TData> {
+  label: string;
+  value: keyof TData;
+  component?: (props: Option) => JSX.Element | null;
+  options?: Option[];
+}
+
+export interface DataTableFilterOption<TData> {
+  id: string;
+  label: string;
+  value: keyof TData;
+  options: Option[];
+  filterValues?: string[];
+  filterOperator?: string;
+  isMulti?: boolean;
+}
