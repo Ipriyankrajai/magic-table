aaf941abd6528ef528781d4c8a50eddfc36e9567
Priyank Rajai <priyank73@hotmail.com>
2024-08-04 15:53:41 +0530

    feat: adding data table to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+"use client";
+
+import { DataTable } from "@/components/data-table";
+import { columns } from "@/components/data-table/column";
+import { data, filterFields } from "@/components/data-table/constants";
+import { schema } from "@/components/data-table/schema";
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const search = schema.safeParse(searchParams);
+
+  if (!search.success) {
+    console.log(search.error);
+    return null;
+  }
+
   return (
-    <main>
-      <h1>Hello</h1>
-    </main>
+    <DataTable
+      columns={columns}
+      data={data}
+      filterFields={filterFields}
+      defaultColumnFilters={Object.entries(search.data).map(([key, value]) => ({
+        id: key,
+        value,
+      }))}
+    />
   );
 }
